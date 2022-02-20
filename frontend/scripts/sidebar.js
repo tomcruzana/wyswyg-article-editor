b69c47a6abede115346f07003d2be4b6b53a8cd5
tomcruzana <tomcruzana@ymail.com>
2022-02-20 03:12:01 -0800

    updated login & logout scripts

--- frontend/scripts/sidebar.js
+++ frontend/scripts/sidebar.js
@@ -1,3 +1,14 @@
+const logOutBtn = document.querySelector(".logout");
+
+logOutBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const res = confirm("Are you sure you want to log out?");
+
+  if (res) {
+    location.href = "login_form.html";
+  }
+});
+
 window.addEventListener("DOMContentLoaded", () => {
   document.querySelector("#sidebarCollapse").addEventListener("click", () => {
     document.querySelector("#sidebar").classList.toggle("active");
